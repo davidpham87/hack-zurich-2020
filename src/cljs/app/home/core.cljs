0eaa41fd598fb4c3d27807652e70631972fe6822
David Pham <davidpham87@gmail.com>
2020-09-19 12:07:05 +0000

    feat: update

--- src/cljs/app/home/core.cljs
+++ src/cljs/app/home/core.cljs
@@ -1,7 +1,7 @@
 (ns app.home.core
   (:require
    ["@material-ui/core/Grid" :default mui-grid]
-   [app.components.mui-utils :refer (card)]
+   [app.components.mui-utils :refer (card left-right)]
    [transparency.components.charts.indicator
     :refer (indicator-chart score->color)]))
 
@@ -17,17 +17,42 @@
      (for [[v title color] (map vector data cols plot-colors)
            :let [card-title (str "Signal of "  title)]]
        ^{:key (gensym "emotion-id")}
-       [:> mui-grid {:item :true :xs 12 :md 6 :lg 4}
+       [:> mui-grid {:item :true :xs 12 :md 4 :lg 4}
         [indicator-chart
          {:value v
           :range [0 100]
           :color color
           :title title
-          :layout {:height 200
+          :layout {:height 220
                    :margin {:l 0 :t 0 :b 0 :r 0}}}]])]))
 
 
+(defn summary-card []
+  [card
+   {:style {:height "99%"}}
+   {:header {:title "Total Growth Points"}
+    :content {:children "1250 Growth Points"
+              :style {:height "80%"}}
+    :actions {:children
+              [:<>
+               [left-right
+                "Last session"
+                "+120"]]}
+    :style {:height "100%"}}])
+
+(defn score-over-time []
+
+  )
+
+
 (defn root []
-  [:> mui-grid {:container true :style {:padding 20}}
+  [:> mui-grid {:container true :style {:padding 20} :spacing 2
+                :align-items :stretch}
+   [:> mui-grid {:item true :xs 12 :lg 8}
+    [:img {:src "img/jasper.png" :style {:width "100%"}}]]
+   [:> mui-grid {:item true :xs 12 :md 12 :lg 4}
+    [summary-card]]
    [:> mui-grid {:item true :xs 12  :lg 8}
-    [indicators]]])
+    [indicators]]
+   [:> mui-grid {:item true :xs 12  :lg 4}
+    "Plot"]])
