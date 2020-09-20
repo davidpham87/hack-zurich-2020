d0223831c5224d13036f2801d423b214df0422c5
David Pham <davidpham87@gmail.com>
2020-09-20 07:50:35 +0000

    feat: update

--- src/cljs/app/team/core.cljs
+++ src/cljs/app/team/core.cljs
@@ -3,7 +3,9 @@
    ["@material-ui/core/Card" :default mui-card]
    ["@material-ui/core/CardMedia" :default mui-card-media]
    ["@material-ui/core/Grid" :default mui-grid]
-   [app.components.mui-utils :refer (card-content card-header)]))
+   [re-frame.core :refer (subscribe)]
+   [app.components.mui-utils :refer (card-content card-header)]
+   [transparency.components.screen-size :as tcs]))
 
 (def team-data
   [{:image 1041
@@ -28,22 +30,27 @@
     :details "Clojure for the win."}])
 
 (defn member-card [{:keys [image title subtitle details]}]
-  [:> mui-card {:style {:padding 10} :elevation 4}
-   [:> mui-grid {:container true}
-    [:> mui-grid {:item true :xs 5 :sm 4 :md 3}
-     [:> mui-card-media
-      {:style {:height "100%" :border-radius "100%"
-               :padding 10}
-       :image (str "https://picsum.photos/id/" image "/150")}]]
-    [:> mui-grid {:item true :xs 7 :sm 8 :md 9}
-     [card-header {:title title :subheader subtitle}]
-     [card-content {:children details}]]]])
+  (let [screen-size @(subscribe [::tcs/screen-size])]
+    [:> mui-card {:style {:padding (if (= screen-size :xs) 1 10)}
+                  :elevation 4}
+     [:> mui-grid {:container true}
+      [:> mui-grid {:item true :xs 5 :sm 4 :md 3}
+       [:> mui-card-media
+        {:style {:height "100%" :border-radius "100%"
+                 :padding 10}
+         :image (str "https://picsum.photos/id/" image "/150")}]]
+      [:> mui-grid {:item true :xs 7 :sm 8 :md 9}
+       [card-header {:title title :subheader subtitle}]
+       [card-content {:children details}]]]]))
 
 (defn root []
-  [:<>
-   [:div {:style {:margin-top 80}}]
-   [:> mui-grid {:container true :spacing 4 :style {:padding 20}
-                 :justify :center}
-    (for [m team-data]
-      [:> mui-grid {:item true :lg 6 :md 8 :sm 10 :xs 11}
-       [member-card m]])]])
+  (let [screen-size @(subscribe [::tcs/screen-size])]
+    [:<>
+     [:div {:style {:margin-top 80}}]
+     [:> mui-grid {:container true :spacing 2
+                   :style {:padding (if (= screen-size :xs) 0 20)}
+                   :justify :center}
+      (for [m team-data]
+        [:> mui-grid {:item true :lg 6 :md 8 :sm 10 :xs 12}
+         [member-card m]])]])
+  )
